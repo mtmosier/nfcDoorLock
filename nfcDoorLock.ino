1b29e9be5f040f7ccc2ed0d9d78902f9ad264abd
Michael T. Mosier <mtmosier@gmail.com>
2015-02-13 17:09:47 -0600

    Making the tag id section a little more straightforward.

--- nfcDoorLock.ino
+++ nfcDoorLock.ino
@@ -2,8 +2,8 @@
 #include <Adafruit_NFCShield_I2C.h>
 
 #define IRQ             2
-#define RESET           3  // Not connected by default on the NFC Shield
-#define VALID_ID_COUNT  2
+#define RESET           3
+#define VALID_ID_COUNT  1  // Number of NFC tag ids you are allowing
 
 const int greenLedPin            =    7;  //  LOW is on
 const int redLedPin              =   12;  //  LOW is on
@@ -20,9 +20,20 @@ typedef struct
 
 //  Replace with the ids of your nfc tags
 RingId validIds[VALID_ID_COUNT] = {
-    { 0x04, 0xcd, 0x74, 0x42, 0x94, 0x33, 0x80 },  //  Mike ring 1 transparent
-    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 }
+    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
 };
+
+/*
+//  If using multiple tags change the VALID_ID_COUNT at the top of the
+//  sketch and change the validIds section above like this: 
+
+RingId validIds[VALID_ID_COUNT] = {
+    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },  //  ID of tag 1
+    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 },  //  ID of tag 2
+    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02 }   //  ID of tag 3
+};
+
+*/
 
 
 Adafruit_NFCShield_I2C nfc(IRQ, RESET);
@@ -64,6 +75,15 @@ void loop(void) {
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
   
   if (success) {
+    Serial.println();
+    Serial.println("NFC Tag detected.  ID is:");
+    Serial.print("{ ");
+    for (int i = 0; i < 7; i++) {
+      printHex(uid[i]);
+      Serial.print(" ");
+    }
+    Serial.println("}");
+
     found = false;
     for (int i = 0; i < VALID_ID_COUNT; i++) {
       if (memcmp(validIds[i].id, uid, 7) == 0) {
@@ -73,7 +93,7 @@ void loop(void) {
     }
 
     if (found) {
-      Serial.println("NFC Tag detected, confirmed as valid");
+      Serial.println("Tag confirmed as valid");
 
       digitalWrite(greenLedPin, LOW);   // turn the green LED on
       digitalWrite(lockPin, HIGH);  // open the door lock
@@ -81,12 +101,20 @@ void loop(void) {
       digitalWrite(lockPin, LOW);   // close the door lock
       digitalWrite(greenLedPin, HIGH);  // turn the green LED off
     } else {
-      Serial.println("NFC Tag detected, but is not in the list of valid IDs");
+      Serial.println("Tag is not in the list of valid IDs.");
 
       digitalWrite(redLedPin, LOW);  // turn the red LED on
       delay(resetTimeOnBadReadInMs);
       digitalWrite(redLedPin, HIGH); // turn the red LED off
     }
   }
+}
+
+void printHex(uint8_t input)
+{
+  Serial.print("0x");
+  if (input < 10)
+    Serial.print("0");
+  Serial.print(input, HEX);
 }
 
